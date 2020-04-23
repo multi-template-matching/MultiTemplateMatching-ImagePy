23b99abd005fe05983c3648c53fa1ab9498379d5
Laurent Thomas <laurent132.thomas@laposte.net>
2020-04-23 16:47:49 +0200

    begin GUI

---
+++ menus/Multi-Template-Matching/detectObjects_plg.py
@@ -0,0 +1,19 @@
+from imagepy.core.engine import Free
+import cv2
+
+class Plugin(Free):
+    title = 'Multi-Template-Matching'
+    para = {'method':cv2.TM_CCOEFF_NORMED, 
+            'Nobject':1,
+			'score-thresh':0.5,
+			'overlap-thresh':0.4} # parameters name with default values
+    
+	
+	view = [
+            (int, 'Nobject', (1,np.inf), 0, 'Expected number of objects', ''),
+			('slide', 'score-thresh', (0, 1), 2, 'Score-threshold'),
+			('slide', 'overlap-thresh', (0, 1), 2, 'Overlap-threshold')
+			] # Add method choice
+
+    def run(self, para=None):
+        IPy.alert('Name:\t%s\r\nAge:\t%d'%(para['name'], para['age']))
