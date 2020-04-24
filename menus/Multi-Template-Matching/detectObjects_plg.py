f72a6806e73c4781dedddc058951293d87f9fe15
Laurent Thomas <laurent132.thomas@laposte.net>
2020-04-24 13:02:04 +0200

    fix indentation

--- menus/Multi-Template-Matching/detectObjects_plg.py
+++ menus/Multi-Template-Matching/detectObjects_plg.py
@@ -5,15 +5,15 @@ class Plugin(Free):
     title = 'Multi-Template-Matching'
     para = {'method':cv2.TM_CCOEFF_NORMED, 
             'Nobject':1,
-			'score-thresh':0.5,
-			'overlap-thresh':0.4} # parameters name with default values
+            'score-thresh':0.5,
+            'overlap-thresh':0.4} # parameters name with default values
     
-	
-	view = [
+    
+    view = [
             (int, 'Nobject', (1,np.inf), 0, 'Expected number of objects', ''),
-			('slide', 'score-thresh', (0, 1), 2, 'Score-threshold'),
-			('slide', 'overlap-thresh', (0, 1), 2, 'Overlap-threshold')
-			] # Add method choice
+            ('slide', 'score-thresh', (0, 1), 2, 'Score-threshold'),
+            ('slide', 'overlap-thresh', (0, 1), 2, 'Overlap-threshold')
+            ] # Add method choice
 
     def run(self, para=None):
         IPy.alert('Name:\t%s\r\nAge:\t%d'%(para['name'], para['age']))
