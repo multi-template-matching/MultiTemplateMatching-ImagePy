cf3874bb762073463c7626a79b7618a6d9dcf36a
Laurent Thomas <laurent132.thomas@laposte.net>
2020-04-27 18:46:21 +0200

    add MTM

--- menus/Multi-Template-Matching/detectObjects_plg.py
+++ menus/Multi-Template-Matching/detectObjects_plg.py
@@ -1,7 +1,7 @@
 from imagepy import IPy
 from imagepy.core.engine  import Free
 from imagepy.core.manager import ImageManager
-import cv2
+import cv2, MTM
 import numpy as np
 
 methods = {"Square-diff": cv2.TM_SQDIFF_NORMED,
@@ -9,14 +9,49 @@ methods = {"Square-diff": cv2.TM_SQDIFF_NORMED,
            "0-mean cross-corr": cv2.TM_CCOEFF_NORMED}
 
 class Plugin(Free):
-    title = 'Multi-Template-Matching'
+    title = 'Compute correlation map'
+    
+    para = {'template':None,
+            'image':None,
+            'method':list(methods.keys())[-1]
+			}
+    
+    
+    view = [
+            ("img", "template", "Template image", ""),
+            ("img", "image", "Target image", ""),
+            (list, "method", list(methods.keys()), str, 'Method for computation of score map', '(normalised)'),
+            ]
+
+    def run(self, para=None):
+        template = ImageManager.get(para["template"]).img # 2D array
+        image    = ImageManager.get(para["image"]).img    # use imgs for a stack
+        
+        method = methods[ para['method'] ] # para['method'] returns the selected string
+        
+		if template.dtype == "float64" or image.dtype == "float64": 
+			raise ValueError("64-bit not supported, max 32-bit")
+        
+        ## Compute correlation map
+        if template.dtype == "uint8" and image.dtype == "uint8":
+            corrMap = cv2.matchTemplate(template, image, method)
+        else:
+            corrMap = cv2.matchTemplate(np.float32(template), np.float32(image), method)
+		
+		IPy.show_img([corrMap], "Correlation map")
+		
+
+
+class Plugin(Free):
+    title = 'Find template'
     
     para = {'template':None,
             'image':None,
             'method':list(methods.keys())[-1], 
             'Nobject':1,
             'score-thresh':0.5,
-            'overlap-thresh':0.4} # parameters name with default values
+            'overlap-thresh':0.4
+			} # parameters name with default values
     
     
     view = [
@@ -29,10 +64,21 @@ class Plugin(Free):
             ]
 
     def run(self, para=None):
-        template = ImageManager.get(para["template"]).img # 2D array
-        image    = ImageManager.get(para["image"]).img    # use imgs for a stack
+        templateTitle = para["template"]
+		template = ImageManager.get(templateTitle).img # 2D array
+        image    = ImageManager.get(para["image"]).img # use imgs for a stack
         
         method = methods[ para['method'] ] # para['method'] returns the selected string
-        #IPy.alert('Name:\t%s\r\nAge:\t%d'%(para['name'], para['age']))
-        corrMap = cv2.matchTemplate(image, template, method) 
-        IPy.show_img([corrMap], "Correlation map")
+		
+		
+		tableHit = MTM.matchTemplates([(templateTile, template)], 
+									  image, 
+									  method,
+									  para["Nobject"]
+									  para["score-thresh"],
+									  para["overlap-thresh"]
+									  )
+		
+		
+		IPy.show_img([corrMap], "Correlation map")
+		IPy.showTable(tableHit, "Multi-Template-Matching detections")
