6b5d3b0a8ba8cc962099168217f5600357c0c51f
Laurent Thomas <laurent132.thomas@laposte.net>
2020-04-27 17:12:58 +0200

    tested functional

--- menus/Multi-Template-Matching/detectObjects_plg.py
+++ menus/Multi-Template-Matching/detectObjects_plg.py
@@ -1,4 +1,6 @@
-from imagepy.core.engine import Free
+from imagepy import IPy
+from imagepy.core.engine  import Free
+from imagepy.core.manager import ImageManager
 import cv2
 import numpy as np
 
@@ -8,19 +10,29 @@ methods = {"Square-diff": cv2.TM_SQDIFF_NORMED,
 
 class Plugin(Free):
     title = 'Multi-Template-Matching'
-    para = {'method':methods.keys()[-1], 
+    
+    para = {'template':None,
+            'image':None,
+            'method':list(methods.keys())[-1], 
             'Nobject':1,
             'score-thresh':0.5,
             'overlap-thresh':0.4} # parameters name with default values
     
     
     view = [
-            (int, 'Nobject', (1,np.inf), 0, 'Expected number of objects', ''),
-            (list, "method", methods.keys(), str, 'Method for computation of score map', '(normalised)')
+            ("img", "template", "Template image", ""),
+            ("img", "image", "Target image", ""),
+            (int, 'Nobject', (1, np.inf), 0, 'Expected number of objects', ''),
+            (list, "method", list(methods.keys()), str, 'Method for computation of score map', '(normalised)'),
             ('slide', 'score-thresh', (0, 1), 2, 'Score-threshold'),
             ('slide', 'overlap-thresh', (0, 1), 2, 'Overlap-threshold')
             ]
 
     def run(self, para=None):
+        template = ImageManager.get(para["template"]).img # 2D array
+        image    = ImageManager.get(para["image"]).img    # use imgs for a stack
+        
         method = methods[ para['method'] ] # para['method'] returns the selected string
         #IPy.alert('Name:\t%s\r\nAge:\t%d'%(para['name'], para['age']))
+        corrMap = cv2.matchTemplate(image, template, method) 
+        IPy.show_img([corrMap], "Correlation map")
