b7dcfb21f4c3afacd2421c96bef678983cb5199f
Laurent Thomas <laurent132.thomas@laposte.net>
2020-04-28 12:35:01 +0200

    Update detectObjects_plg.py

--- menus/Multi-Template-Matching/detectObjects_plg.py
+++ menus/Multi-Template-Matching/detectObjects_plg.py
@@ -8,13 +8,13 @@ methods = {"Square-diff": cv2.TM_SQDIFF_NORMED,
            "cross-corr":  cv2.TM_CCORR_NORMED,
            "0-mean cross-corr": cv2.TM_CCOEFF_NORMED}
 
-class Plugin(Free):
+class ComputeMap(Free):
     title = 'Compute correlation map'
     
     para = {'template':None,
             'image':None,
             'method':list(methods.keys())[-1]
-			}
+            }
     
     
     view = [
@@ -29,21 +29,21 @@ class Plugin(Free):
         
         method = methods[ para['method'] ] # para['method'] returns the selected string
         
-		if template.dtype == "float64" or image.dtype == "float64": 
-			raise ValueError("64-bit not supported, max 32-bit")
+        if template.dtype == "float64" or image.dtype == "float64": 
+            raise ValueError("64-bit not supported, max 32-bit")
         
         ## Compute correlation map
         if template.dtype == "uint8" and image.dtype == "uint8":
             corrMap = cv2.matchTemplate(template, image, method)
         else:
             corrMap = cv2.matchTemplate(np.float32(template), np.float32(image), method)
-		
-		IPy.show_img([corrMap], "Correlation map")
-		
+        
+        IPy.show_img([corrMap], "Correlation map")
+        
 
 
-class Plugin(Free):
-    title = 'Find template'
+class MultiTempMatching(Free):
+    title = 'Multi-template-matching'
     
     para = {'template':None,
             'image':None,
@@ -51,7 +51,7 @@ class Plugin(Free):
             'Nobject':1,
             'score-thresh':0.5,
             'overlap-thresh':0.4
-			} # parameters name with default values
+            } # parameters name with default values
     
     
     view = [
@@ -65,20 +65,19 @@ class Plugin(Free):
 
     def run(self, para=None):
         templateTitle = para["template"]
-		template = ImageManager.get(templateTitle).img # 2D array
+        template = ImageManager.get(templateTitle).img # 2D array
         image    = ImageManager.get(para["image"]).img # use imgs for a stack
         
         method = methods[ para['method'] ] # para['method'] returns the selected string
-		
-		
-		tableHit = MTM.matchTemplates([(templateTile, template)], 
-									  image, 
-									  method,
-									  para["Nobject"]
-									  para["score-thresh"],
-									  para["overlap-thresh"]
-									  )
-		
-		
-		IPy.show_img([corrMap], "Correlation map")
-		IPy.showTable(tableHit, "Multi-Template-Matching detections")
+        
+        
+        tableHit = MTM.matchTemplates([(templateTitle, template)], 
+                                      image, 
+                                      method,
+                                      para["Nobject"],
+                                      para["score-thresh"],
+                                      para["overlap-thresh"]
+                                      )
+        
+        
+        IPy.showTable(tableHit, "Multi-Template-Matching detections")
